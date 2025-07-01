476804b491f94787a65610975f3711dfd9f88f28
Martin Visser <martinvisser@users.noreply.github.com>
2025-07-01 08:51:35 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    kotlin("jvm") version "2.1.21"
+    id("com.netflix.dgs.codegen") version "8.1.0"
+}
+
+group = "com.example.dgscodegeninterfaces"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.18.3")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.generateJava {
+    schemaPaths = mutableListOf("${projectDir}/src/main/resources/schema") // List of directories containing schema files
+    packageName = "com.example.dgscodegeninterfaces.generated" // The package name to use to generate sources
+    generateClientv2 = true // Enable generating the type safe query API
+    generateDataTypes = true
+}
